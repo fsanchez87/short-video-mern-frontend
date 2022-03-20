14aac9d04d5ca0ee1f53a02966aa1f6c64948c69
Francisco S <sanchezgarridof@gmail.com>
2022-03-20 16:49:14 +0100

    Making Components Dynamic

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ function App() {
   return (
     <div className="app">
       <div className="app__videos">
-        <Video />
-        <Video />
+        <Video
+          url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
+          channel="nabendu82"
+          description="Macbook Air to new Windows editing beast"
+          song="I am a Windows PC"
+          likes={345}
+          shares={200}
+          messages={90}
+        />
+        <Video
+          url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
+          channel="nabendu82"
+          description="Macbook Air to new Windows editing beast"
+          song="I am a Windows PC"
+          likes={35}
+          shares={20}
+          messages={9}
+        />
+        D
       </div>
     </div>
   );
--- src/components/VideoFooter.jsx
+++ src/components/VideoFooter.jsx
@@ -3,18 +3,18 @@ import "./VideoFooter.css";
 import MusicNoteIcon from "@material-ui/icons/MusicNote";
 import Ticker from "react-ticker";
 
-const VideoFooter = () => {
+const VideoFooter = ({ chanel, description, song }) => {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
-        <h3>@nabendu82</h3>
-        <p>Macbook Air to new Windows editing beast</p>
+        <h3>@{chanel}</h3>
+        <p>{description}</p>
         <div className="videoFooter__ticker">
           <MusicNoteIcon className="videoFooter__icon" />
           <Ticker mode="smooth">
             {({ index }) => (
               <>
-                <p>I am a Windows PC</p>
+                <p>{song}</p>
               </>
             )}
           </Ticker>
